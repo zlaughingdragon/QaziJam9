edfd5f7b83ed73fd4c9e3c21e38e7b6bf16e3862
Oden <ops991923@gmail.com>
2016-12-09 18:17:35 -0800

    Initial project

---
+++ Assets/Scripts/Menu/ButtonPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPlay : MonoBehaviour
+{
+	public string sceneName = "Game";
+
+	public void OnClick()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# QaziJam9
